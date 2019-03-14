41096e9306f9fbf827932e867ad475bf8bfdc4ae
matt_moe <mrobertmoe@gmail.com>
2019-03-14 12:04:10 +0700

    start component did mount update on collection update

--- react-firestore/src/App.js
+++ react-firestore/src/App.js
@@ -4,6 +4,35 @@ import './App.css';
 import firebase from './Firebase';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection('boards');
+    this.unsubscribe = null;
+    this.state = {
+      boards: []
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const boards = [];
+    querySnapshot.forEach((doc) => {
+      const {title, description, author } = doc.data();
+      boards.push({
+        key: doc.id,
+        doc, //Document snapshot
+        title,
+        description,
+        author,
+      });
+    });
+    this.setState({
+      boards
+    });
+  }
+  //refreshes and shows changes when board collections update
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
   render() {
     return (
       <div className="App">
