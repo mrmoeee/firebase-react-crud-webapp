7eb1d374a6ea9a298fd865e4d3357d32657be1de
matt_moe <mrobertmoe@gmail.com>
2019-03-14 12:40:29 +0700

    created Create form for adding boards

---
+++ react-firestore/src/components/Create.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import firebase from '../Firebase';
+import { Link } from 'react-router-dom';
+
+class Create extends Component {
+
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('boards');
+    this.state = { 
+      title: '',
+      description: '',
+      author: ''
+    };
+  }
+
+  onChange = (e) => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { title, description, author } = this.state;
+
+    this.ref.add({
+      title,
+      description,
+      author
+    }).then((docRef) => {
+      this.setState({
+        title: '',
+        description: '',
+        author: ''
+      });
+      this.props.history.push("/");
+    }).catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+  }
+
+  render() {
+    const { title, description, author } = this.state;
+    return (
+      <div className="container">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">ADD BOARD</h3>
+          </div>
+          <div className="panel-body">
+            <h4><Link to="/" class="btn btn-primary">Book Lists</Link></h4>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label for="title">Title:</label>
+                <input type="text" class="form-control" name="title" value={title} onChange={this.onChange} placeholder="Title" />
+              </div>
+              <div className="form-group">
+                <label for="description">Description:</label>
+                <textarea class="form-control" name="description" onChange={this.onChange} placeholder="Description">{description}</textarea>
+               </div>
+              <div className="form-group">
+                <label for="author">Author:</label>
+                <input type="text" class="form-control" name="author" value={author} onChange={this.onChange} placeholder="Title" />
+              </div>
+              <button type="submit" class="btn btn-succes">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Create;
